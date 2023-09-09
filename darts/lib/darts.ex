a26b1182e589375b18d9f724f98761d26b9687fc
ktunprasert <ktunprasert@outlook.com>
2023-09-09 15:40:28 +0100

    darts

---
+++ darts/lib/darts.ex
@@ -0,0 +1,18 @@
+defmodule Darts do
+  @type position :: {number, number}
+
+  @doc """
+  Calculate the score of a single dart hitting a target
+  """
+  @spec score(position) :: integer
+  def score({x, y}) do
+    dist = (x * x + y * y) ** 0.5
+
+    cond do
+      dist > 10 -> 0
+      dist > 5 -> 1
+      dist > 1 -> 5
+      true -> 10
+    end
+  end
+end
