3109fee14be0b4a4a7f0e94f92f740507e223918
ktunprasert <ktunprasert@outlook.com>
2023-09-09 17:22:15 +0100

    leap: when guard cases

--- leap/lib/year.ex
+++ leap/lib/year.ex
@@ -9,10 +9,8 @@ defmodule Year do
       unless the year is also evenly divisible by 400
   """
   @spec leap_year?(non_neg_integer) :: boolean
-  def leap_year?(year) do
-    case {rem(year, 100), rem(year, 400)} do
-      {0, x} when x != 0 -> false
-      _ -> rem(year, 4) == 0
-    end
-  end
+  def leap_year?(y) when rem(y, 400) == 0, do: true
+  def leap_year?(y) when rem(y, 100) == 0, do: false
+  def leap_year?(y) when rem(y, 4) == 0, do: true
+  def leap_year?(_), do: false
 end
