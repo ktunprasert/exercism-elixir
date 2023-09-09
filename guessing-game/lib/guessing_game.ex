c487956e8d5c4702c119f6df8785d3bd1461511d
ktunprasert <ktunprasert@outlook.com>
2023-09-09 15:53:44 +0100

    guessing-game: default value must be used

--- guessing-game/lib/guessing_game.ex
+++ guessing-game/lib/guessing_game.ex
@@ -1,9 +1,8 @@
 defmodule GuessingGame do
-  def compare(_), do: "Make a guess"
+  def compare(_, guess \\ :no_guess)
   def compare(_, :no_guess), do: "Make a guess"
-  def compare(secret, guess) when secret == guess, do: "Correct"
-  def compare(secret, guess) when secret == guess - 1, do: "So close"
-  def compare(secret, guess) when secret == guess + 1, do: "So close"
+  def compare(secret, secret), do: "Correct"
+  def compare(secret, guess) when abs(secret - guess) == 1, do: "So close"
   def compare(secret, guess) when secret > guess, do: "Too low"
   def compare(secret, guess) when secret < guess, do: "Too high"
 end
