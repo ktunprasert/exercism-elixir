d9de3eebab911446c2b915a8670b2ee5c0d507d4
ktunprasert <ktunprasert@outlook.com>
2023-09-09 14:37:04 +0100

    freelancer-rates

---
+++ freelancer-rates/lib/freelancer_rates.ex
@@ -0,0 +1,25 @@
+defmodule FreelancerRates do
+  @hours 8.0
+  @days 22.0
+
+  def daily_rate(hourly_rate) do
+    @hours * hourly_rate
+  end
+
+  def apply_discount(before_discount, discount) do
+    before_discount * (1 - discount / 100)
+  end
+
+  def monthly_rate(hourly_rate, discount) do
+    hourly_rate
+    |> daily_rate
+    |> Kernel.*(@days)
+    |> apply_discount(discount)
+    |> ceil
+  end
+
+  def days_in_budget(budget, hourly_rate, discount) do
+    (budget / (hourly_rate |> daily_rate |> apply_discount(discount)))
+    |> Float.floor(1)
+  end
+end
