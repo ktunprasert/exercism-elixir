02a74838fd08f6f9614fa608d9bc3b0fc96564c3
ktunprasert <ktunprasert@outlook.com>
2023-09-10 20:02:18 +0100

    wine-cellar

---
+++ wine-cellar/lib/wine_cellar.ex
@@ -0,0 +1,46 @@
+defmodule WineCellar do
+  def explain_colors do
+    [
+      {:white, "Fermented without skin contact."},
+      {:red, "Fermented with skin contact using dark-colored grapes."},
+      {:rose, "Fermented with some skin contact, but not enough to qualify as a red wine."}
+    ]
+  end
+
+  def filter([], _), do: []
+
+  def filter(cellar, color, opts \\ []) do
+    cellar
+    |> Keyword.take([color])
+    |> Keyword.values()
+    |> maybe_filter(opts[:year], &filter_by_year/2)
+    |> maybe_filter(opts[:country], &filter_by_country/2)
+  end
+
+  def maybe_filter(wines, nil, _), do: wines
+  def maybe_filter(wines, arg, func), do: wines |> func.(arg)
+
+  # The functions below do not need to be modified.
+
+  defp filter_by_year(wines, year)
+  defp filter_by_year([], _year), do: []
+
+  defp filter_by_year([{_, year, _} = wine | tail], year) do
+    [wine | filter_by_year(tail, year)]
+  end
+
+  defp filter_by_year([{_, _, _} | tail], year) do
+    filter_by_year(tail, year)
+  end
+
+  defp filter_by_country(wines, country)
+  defp filter_by_country([], _country), do: []
+
+  defp filter_by_country([{_, _, country} = wine | tail], country) do
+    [wine | filter_by_country(tail, country)]
+  end
+
+  defp filter_by_country([{_, _, _} | tail], country) do
+    filter_by_country(tail, country)
+  end
+end
