734d2ad120f4d4d0d80ad6ee3fa6ec3d5695ab58
ktunprasert <ktunprasert@outlook.com>
2023-09-10 17:29:26 +0100

    take-a-number

---
+++ take-a-number/lib/take_a_number.ex
@@ -0,0 +1,23 @@
+defmodule TakeANumber do
+  def start() do
+    spawn(fn -> loop(0) end)
+  end
+
+  defp loop(x) do
+    receive do
+      {:report_state, sender_pid} ->
+        send(sender_pid, x)
+        loop(x)
+
+      {:take_a_number, sender_pid} ->
+        send(sender_pid, x + 1)
+        loop(x + 1)
+
+      :stop ->
+        :ok
+
+      _ ->
+        loop(x)
+    end
+  end
+end
