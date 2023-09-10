222c6ee000a47bd9d778b5131b4fa4d0e32b4756
ktunprasert <ktunprasert@outlook.com>
2023-09-10 16:03:39 +0100

    binary-search

---
+++ binary-search/lib/binary_search.ex
@@ -0,0 +1,36 @@
+defmodule BinarySearch do
+  @doc """
+    Searches for a key in the tuple using the binary search algorithm.
+    It returns :not_found if the key is not in the tuple.
+    Otherwise returns {:ok, index}.
+
+    ## Examples
+
+      iex> BinarySearch.search({}, 2)
+      :not_found
+
+      iex> BinarySearch.search({1, 3, 5}, 2)
+      :not_found
+
+      iex> BinarySearch.search({1, 3, 5}, 5)
+      {:ok, 2}
+
+  """
+
+  @spec search(tuple, integer) :: {:ok, integer} | :not_found
+  def search({}, _), do: :not_found
+
+  def search(numbers, key) do
+    search(numbers, key, 0, (Tuple.to_list(numbers) |> length()) - 1)
+  end
+
+  defp search(numbers, key, low, high) do
+    mid = div(low + high, 2)
+
+    case elem(numbers, mid) do
+      ^key -> {:ok, mid}
+      v when v < key -> search(numbers, key, mid + 1, high)
+      _ -> search(numbers, key, low, mid - 1)
+    end
+  end
+end
