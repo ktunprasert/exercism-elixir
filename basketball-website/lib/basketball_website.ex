265c42d99ea4120b78f72038423261796b762deb
ktunprasert <ktunprasert@outlook.com>
2023-09-11 22:39:06 +0100

    basketball-website: smart enum reduce

--- basketball-website/lib/basketball_website.ex
+++ basketball-website/lib/basketball_website.ex
@@ -1,16 +1,6 @@
 defmodule BasketballWebsite do
-  def extract_from_path(data, []), do: data
-
-  def extract_from_path(data, [head | rest]) do
-    if data[head] != nil do
-      extract_from_path(data[head], rest)
-    else
-      nil
-    end
-  end
-
   def extract_from_path(data, path) when is_binary(path) do
-    extract_from_path(data, String.split(path, "."))
+    Enum.reduce(String.split(path, "."), data, & &2[&1])
   end
 
   def get_in_path(data, path) do
