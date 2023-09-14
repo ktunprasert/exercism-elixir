dd0b7b5415ea5d4ff1edd00773d23eec75a232bc
ktunprasert <ktunprasert@outlook.com>
2023-09-15 00:31:08 +0100

    acronym

---
+++ acronym/lib/acronym.ex
@@ -0,0 +1,13 @@
+defmodule Acronym do
+  @doc """
+  Generate an acronym from a string.
+  "This is a string" => "TIAS"
+  """
+  @spec abbreviate(String.t()) :: String.t()
+  def abbreviate(string) do
+    String.split(string, ~r/\W+/)
+    |> Enum.map(&String.first/1)
+    |> Enum.map(&String.upcase/1)
+    |> Enum.join()
+  end
+end
