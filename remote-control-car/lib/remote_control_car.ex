17574e271a2c37e0b2ef144d8d1da3a5c4e53527
ktunprasert <ktunprasert@outlook.com>
2023-09-20 18:25:40 +0100

    remote-control-car

---
+++ remote-control-car/lib/remote_control_car.ex
@@ -0,0 +1,41 @@
+defmodule RemoteControlCar do
+  @enforce_keys [:nickname]
+  defstruct [:battery_percentage, :distance_driven_in_meters, :nickname]
+
+  def new() do
+    %RemoteControlCar{
+      battery_percentage: 100,
+      distance_driven_in_meters: 0,
+      nickname: "none"
+    }
+  end
+
+  def new(nickname) do
+    %{new() | nickname: nickname}
+  end
+
+  def display_distance(remote_car) when is_struct(remote_car, RemoteControlCar) do
+    remote_car
+    |> Map.get(:distance_driven_in_meters)
+    |> then(&"#{&1} meters")
+  end
+
+  def display_battery(remote_car) when is_struct(remote_car, RemoteControlCar) do
+    remote_car
+    |> Map.get(:battery_percentage)
+    |> then(fn
+      0 -> "Battery empty"
+      p -> "Battery at #{p}%"
+    end)
+  end
+
+  def drive(remote_car) when is_struct(remote_car, RemoteControlCar) do
+    do_drive(remote_car)
+  end
+
+  def do_drive(%{battery_percentage: 0} = car), do: car
+
+  def do_drive(%{distance_driven_in_meters: dist, battery_percentage: battery} = car) do
+    %{car | distance_driven_in_meters: dist + 20, battery_percentage: battery - 1}
+  end
+end
