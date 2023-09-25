a18d5fe7caa1654996f8e3bf62203b075b63d0ea
ktunprasert <ktunprasert@outlook.com>
2023-09-25 17:12:17 +0100

    raindrops

---
+++ raindrops/lib/raindrops.ex
@@ -0,0 +1,36 @@
+defmodule Raindrops do
+  @doc """
+  Returns a string based on raindrop factors.
+
+  - If the number contains 3 as a prime factor, output 'Pling'.
+  - If the number contains 5 as a prime factor, output 'Plang'.
+  - If the number contains 7 as a prime factor, output 'Plong'.
+  - If the number does not contain 3, 5, or 7 as a prime factor,
+    just pass the number's digits straight through.
+  """
+  @spec convert(pos_integer) :: String.t()
+  def convert(number) do
+    case pling(number) <> plang(number) <> plong(number) do
+      "" -> Integer.to_string(number)
+      result -> result
+    end
+  end
+
+  defp pling(x) when rem(x, 3) == 0 do
+    "Pling"
+  end
+
+  defp pling(_), do: ""
+
+  defp plang(x) when rem(x, 5) == 0 do
+    "Plang"
+  end
+
+  defp plang(_), do: ""
+
+  defp plong(x) when rem(x, 7) == 0 do
+    "Plong"
+  end
+
+  defp plong(_), do: ""
+end
