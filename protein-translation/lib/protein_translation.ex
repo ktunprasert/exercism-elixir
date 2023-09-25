2e2bb59916bf5c9f67c020708daecde255793003
ktunprasert <ktunprasert@outlook.com>
2023-09-25 17:02:11 +0100

    protein-translation

---
+++ protein-translation/lib/protein_translation.ex
@@ -0,0 +1,79 @@
+defmodule ProteinTranslation do
+  @codons %{
+    "AUG" => "Methionine",
+    "UUU" => "Phenylalanine",
+    "UUC" => "Phenylalanine",
+    "UUA" => "Leucine",
+    "UUG" => "Leucine",
+    "UCU" => "Serine",
+    "UCC" => "Serine",
+    "UCA" => "Serine",
+    "UCG" => "Serine",
+    "UAU" => "Tyrosine",
+    "UAC" => "Tyrosine",
+    "UGU" => "Cysteine",
+    "UGC" => "Cysteine",
+    "UGG" => "Tryptophan",
+    "UAA" => "STOP",
+    "UAG" => "STOP",
+    "UGA" => "STOP"
+  }
+
+  @doc """
+  Given an RNA string, return a list of proteins specified by codons, in order.
+  """
+  @spec of_rna(String.t()) :: {:ok, list(String.t())} | {:error, String.t()}
+  def of_rna(rna) when rem(length(rna), 3) != 0 do
+    {:error, "Invalid RNA"}
+  end
+
+  def of_rna(rna) do
+    rna
+    |> String.to_charlist()
+    |> Enum.chunk_every(3)
+    |> Enum.reduce_while(
+      {:ok, []},
+      fn x, {stat, acc} ->
+        case x |> List.to_string() |> of_codon() do
+          {:ok, "STOP"} ->
+            {:halt, {stat, acc}}
+
+          {:ok, protein} ->
+            {:cont, {stat, acc ++ [protein]}}
+
+          {:error, _} ->
+            {:halt, {:error, "invalid RNA"}}
+        end
+      end
+    )
+  end
+
+  @doc """
+  Given a codon, return the corresponding protein
+
+  UGU -> Cysteine
+  UGC -> Cysteine
+  UUA -> Leucine
+  UUG -> Leucine
+  AUG -> Methionine
+  UUU -> Phenylalanine
+  UUC -> Phenylalanine
+  UCU -> Serine
+  UCC -> Serine
+  UCA -> Serine
+  UCG -> Serine
+  UGG -> Tryptophan
+  UAU -> Tyrosine
+  UAC -> Tyrosine
+  UAA -> STOP
+  UAG -> STOP
+  UGA -> STOP
+  """
+  @spec of_codon(String.t()) :: {:ok, String.t()} | {:error, String.t()}
+  def of_codon(codon) do
+    case Map.get(@codons, codon) do
+      nil -> {:error, "invalid codon"}
+      protein -> {:ok, protein}
+    end
+  end
+end
